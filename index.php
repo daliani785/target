fd8c2de08ceb1e110aab4c6bde3b562793c4627d
daliani <dalianinafeldaa@gmail.com>
2026-02-04 20:24:29 +0700

    update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
     <div class="card">
         <h1>My Target Planner</h1>
-        <p>Mudahkan urusan dengan mencatat setiap targetmu</p>
+        <p>Mudahkan setiap urusan dengan mencatat setiap targetmu</p>
 
         <!-- form inputan -->
         <form action="prosesinput.php" method="post">
